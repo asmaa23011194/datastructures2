fa8971df65829e8260332292eaa19bc12626cab4
asmaa23011194 <167802833+asmaa23011194@users.noreply.github.com>
2024-04-23 00:30:01 +0200

    Add files via upload

---
+++ DatastructuresProject.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datastructures.project;
+
+import javax.xml.parsers.DocumentBuilder; 
+import javax.xml.parsers.DocumentBuilderFactory;
+
+/*
+What it does: Helps you create a tool (parser) for reading XML files.
+How it works: You ask it for a parser using newInstance().
+Why it's useful: It lets you read XML files into a format that Java can understand and work with
+*/
+
+import javax.xml.parsers.ParserConfigurationException;  
+
+/*
+What it is: An error that happens when there's a problem setting up the XML file reader.
+When it's used: If something goes wrong while setting up the XML reader, Java throws this error to let you know.
+How to deal with it: You handle it in your code, usually by fixing the configuration issue or letting the user know something went wrong
+*/
+
+import org.w3c.dom.Document;    
+import org.xml.sax.SAXException;
+
+public class DatastructuresProject {
+
+     public static void main(String[] args) throws paprserConfigrationException, SAXException, IOException {
+         
+      DocumentBuilderFactory factory= DocumentBuilder ; 
+    }
+    
+}
